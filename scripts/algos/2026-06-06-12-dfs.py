58df375d81e3b953cc0ce4821aa0118759f614c3
King-Austin <nworahebuka.a@gmail.com>
2026-06-06 12:07:01 +0000

    2026-06-06 12:07

---
+++ scripts/algos/2026-06-06-12-dfs.py
@@ -0,0 +1,14 @@
+"""
+Depth-First Search
+Generated: 2026-06-06 12:07 UTC
+"""
+
+def dfs(graph: dict, start, visited=None) -> list:
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    order = [start]
+    for neighbour in graph.get(start, []):
+        if neighbour not in visited:
+            order.extend(dfs(graph, neighbour, visited))
+    return order
