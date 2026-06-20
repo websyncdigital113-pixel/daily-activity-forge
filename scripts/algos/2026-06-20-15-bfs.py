fe5a931db506bb8045635f18615e5edb97cc4fb2
King-Austin <nworahebuka.a@gmail.com>
2026-06-20 15:23:34 +0000

    2026-06-20 15:23

---
+++ scripts/algos/2026-06-20-15-bfs.py
@@ -0,0 +1,18 @@
+"""
+Breadth-First Search
+Generated: 2026-06-20 15:23 UTC
+"""
+
+from collections import deque
+
+def bfs(graph: dict, start) -> list:
+    visited, queue, order = set(), deque([start]), []
+    visited.add(start)
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+        for neighbour in graph.get(node, []):
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+    return order
