949c3bf31d6267b3244c734c1bc776c86c382267
King-Austin <nworahebuka.a@gmail.com>
2026-06-24 15:10:23 +0000

    2026-06-24 15:10

---
+++ scripts/algos/2026-06-24-15-union_find.py
@@ -0,0 +1,25 @@
+"""
+Union-Find (Disjoint Set)
+Generated: 2026-06-24 15:10 UTC
+"""
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x: int, y: int) -> bool:
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.parent[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        return True
