e7fcece03c78cedc0b3a1732f71db5d0e1091f67
King-Austin <nworahebuka.a@gmail.com>
2026-07-08 12:06:33 +0000

    2026-07-08 12:06

---
+++ scripts/algos/2026-07-08-12-binary_search.py
@@ -0,0 +1,16 @@
+"""
+Binary Search
+Generated: 2026-07-08 12:06 UTC
+"""
+
+def binary_search(arr: list, target: int) -> int:
+    lo, hi = 0, len(arr) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return -1
