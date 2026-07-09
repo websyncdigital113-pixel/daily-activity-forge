e47941fe4f54279edcebd3c74025b64adf3fb578
King-Austin <nworahebuka.a@gmail.com>
2026-07-09 15:35:32 +0000

    2026-07-09 15:35

---
+++ scripts/algos/2026-07-09-15-merge_sort.py
@@ -0,0 +1,22 @@
+"""
+Merge Sort
+Generated: 2026-07-09 15:35 UTC
+"""
+
+def merge_sort(arr: list) -> list:
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i]); i += 1
+        else:
+            result.append(right[j]); j += 1
+    return result + left[i:] + right[j:]
